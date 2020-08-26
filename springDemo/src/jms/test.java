5cc4077ee8ea14bde030847e0d39d01d6eef40cf
sami meskaoui <sami.meskaoui.sol@gmail.com>
2020-08-26 17:09:38 +0200

    -Spring Framework Trainning

---
+++ springDemo/src/jms/test.java
@@ -0,0 +1,16 @@
+package jms;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+
+
+public class test implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		
+		
+		
+	}
+}
